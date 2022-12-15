b95048f097da5c92ff7c3d40f889082fb3dfb062
eggsnblakey <jphabc54@gmail.com>
2022-12-15 16:32:26 -0500

    update w-tweep

--- readtext-test.R
+++ readtext-test.R
@@ -1,7 +1,19 @@
-library(readtext)
 require(readtext)
+require(quanteda)
+library(quanteda.textplots)
 
 ## Loading required package: readtext
 # get the data directory from readtext
 
 DATA_DIR <- system.file("txt/", package = "readtext")
+
+rt_xi <- readtext(paste0(DATA_DIR, "txt/*"), text_field = "texts")
+
+corpus_xi <- corpus(rt_xi)
+
+dfm_xi <- corpus_subset(corpus_xi) %>%
+  dfm(remove = stopwords('english'), remove_punct = TRUE) %>%
+  dfm_trim(min_termfreq = 20, verbose = FALSE)
+
+set.seed(100)
+textplot_wordcloud(dfm_xi)
