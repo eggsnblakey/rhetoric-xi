ad7e53f960f3d61f80e9fca526df17d466437fb4
eggsnblakey <jphabc54@gmail.com>
2022-12-15 16:35:23 -0500

    new tweeps update

---
+++ tweeps/tweeps-scr.R
@@ -0,0 +1,7 @@
+library(rtweet)
+library(ggplot2)
+library(tidyverse)
+auth <- rtweet_app()
+tmls <- get_timelines(c("ChineseEmbinUS"), n = 100)
+tmls = data.frame(lapply(tmls, as.character), stringsAsFactors=FALSE)
+write.csv(tmls, file = "tweeps/export/burpy.csv")
