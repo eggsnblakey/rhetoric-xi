ad49c8cbfd3d33f4bf7482ec2e883777e0bc91fa
eggsnblakey <jphabc54@gmail.com>
2022-12-15 16:52:51 -0500

    trying to fix

--- tweeps/tweeps-scr.R
+++ tweeps/tweeps-scr.R
@@ -2,6 +2,10 @@ library(rtweet)
 library(ggplot2)
 library(tidyverse)
 auth <- rtweet_app()
+<<<<<<< HEAD
 tmls <- get_timelines(c("ChineseEmbinUS"), n = 100)
+=======
+tmls <- get_timelines(c("ChineseEmbinUS"), n = 1000)
+>>>>>>> b5d8dcf (update)
 tmls = data.frame(lapply(tmls, as.character), stringsAsFactors=FALSE)
 write.csv(tmls, file = "tweeps/export/burpy.csv")
