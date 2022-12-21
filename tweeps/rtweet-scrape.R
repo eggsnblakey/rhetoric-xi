2f749502a0f2252669ba23b2e6874902ac93759a
eggsnblakey <jphabc54@gmail.com>
2022-12-20 21:31:01 -0500

    fix tweeps

---
+++ tweeps/rtweet-scrape.R
@@ -0,0 +1,28 @@
+library(rtweet)
+library(dplyr)
+library(readr)
+
+screen_names <- c("SpokespersonCHN", "zlj517", "HuXijin_GT", "MFA_China", "zhang_heqing", "ReginaIplau", "globaltimesnews")
+
+auth <- rtweet_app()
+
+folder_path <- file.path("tweeps/accts")
+
+# Retrieve the timelines of the accounts
+for (screen_name in screen_names) {
+  # Retrieve the timeline of the current account
+  
+  timeline <- get_timeline(screen_name, n = 1500, include_rts = FALSE)
+  
+  user_timeline <- cbind(timeline, users_data(timeline)[, c("screen_name")])
+  
+  tweets_subset <- select(user_timeline, created_at, full_text, retweet_count, favorite_count, screen_name)
+  
+  # Construct the file path
+  
+  filepath <- file.path(folder_path, paste0(screen_name, ".csv"))
+  
+  # Export the data frame as a CSV file
+  
+  write_csv(tweets_subset, filepath)  
+}
