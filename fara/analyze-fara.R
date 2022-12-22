31ed3404c2c0d76f588f5d03ed61a6cfb118975f
eggsnblakey <jphabc54@gmail.com>
2022-12-22 16:30:17 -0500

    Trying to sync

--- fara/analyze-fara.R
+++ fara/analyze-fara.R
@@ -2,12 +2,21 @@ library(tidyverse)
 library(rvest)
 
 # Read in the URL list from a CSV file
-url_list <- read.csv("url_list2.csv")
+url_list <- read.csv("fara/china-blank2.csv")
 
 # Loop through each URL and download the PDF
 for (i in 1:nrow(url_list)) {
   url <- url_list[i, "url"]
   filename <- url_list[i, "filename"]
-  download.file(url, destfile = filename, mode = "wb")
+  
+  # Specify the path to the subfolder where you want to save the files
+  filepath <- paste0("fara/registrations/pdfs/", filename)
+  
+  download.file(url, destfile = filepath, mode = "wb")
+  
+  # Take a 10 second break every 5 requests
+  if (i %% 5 == 0) {
+    Sys.sleep(10)
+  }
 }
 
